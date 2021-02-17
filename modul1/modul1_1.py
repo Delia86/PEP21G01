d183f1a15e43b1cd7647b91c3e220d996349933b
Delia <deliadragos@gmailcm>
2021-02-17 21:35:57 +0200

    Modul 1

---
+++ modul1/modul1_1.py
@@ -0,0 +1,14 @@
+print('print\u200b("")')
+print('Delia')
+
+print('My name is:','Delia', '1', sep='_',end='*')
+print('My name is:','Delia', '2', sep='_',end='#')
+print('My name is:','Delia', '3', sep='_', end='@')
+print()
+print_text1=123
+print_text2=345
+print(print_text1 ,print_text2)
+
+print(' * ')
+print ('* *')
+print('****')
